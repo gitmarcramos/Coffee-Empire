ef00dd5e85301ca811f81d98c0011c6b4c3796d4
Marc Ramos <59534439+gitmarcramos@users.noreply.github.com>
2021-09-13 00:25:24 +0200

    second commit

---
+++ javascript/app.js
@@ -0,0 +1,14 @@
+
+//! BURGER INTERACTION
+const burger = document.querySelector('#burger');
+const navbar = document.querySelector('#navbar');
+const menu = document.querySelector('#nav-bar-menu');
+
+burger.addEventListener('click', () => {
+    burger.classList.toggle('close');
+    navbar.classList.toggle('nav-closed')
+})
+
+menu.addEventListener('click', () => {
+    window.location.href = "/index.html";
+})
